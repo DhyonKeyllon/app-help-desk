e65e75b3da8d49e1fa7a435ad4fe1c3d6072ae9c
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2021-04-27 21:20:17 -0300

    Criando sessao
    Criando sessao e protegendo paginas restritas

--- index.php
+++ index.php
@@ -39,6 +39,7 @@
                 <div class="form-group">
                   <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+
                 <?php
                   if (isset($_GET['login']) && $_GET['login'] == 'erro') {                     
                 ?>
@@ -46,6 +47,15 @@
                     Usuário ou senha inválido(s)
                   </div>
                 <?php } ?>
+
+                <?php
+                  if (isset($_GET['login']) && $_GET['login'] == 'erro2') {                     
+                ?>
+                  <div class="text-danger">
+                    Faça login antes de acessar as páginas protegidas
+                  </div>
+                <?php } ?>
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- valida_login.php
+++ valida_login.php
@@ -1,4 +1,7 @@
 <?php 
+
+    session_start();
+
     //variavel que verificar se a autenticacao foi realizada
     $usuario_autenticado = false;
 
@@ -22,8 +25,10 @@
 
     if ($usuario_autenticado) {
         echo 'Usuario autenticado';
+        $_SESSION['autenticado'] = 'SIM';
     } else {
         header('Location: index.php?login=erro');
+        $_SESSION['autenticado'] = 'NAO';
     }
 
     /*
